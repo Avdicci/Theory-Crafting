161a65505396c2cb8decb6b3bf23815581220e16
Mirko Keiner <mirko.keiner1@gmail.com>
2025-03-31 01:00:33 +0200

    feat: Added Assassin Class

--- damage_functions.py
+++ damage_functions.py
@@ -1,5 +1,11 @@
 # damage_functions.py
 
+def calculate_damage_assassin(level, damage, hp, energy, final_damage):
+    base_dmg = (2 * level + damage) * (level / 9)
+    hp_influence = ((hp / 20) * (energy / 40)) * 0.5
+    boosted_damage = (1.2 * base_dmg) + hp_influence
+    return boosted_damage * final_damage
+
 def calculate_damage_bishop(level, damage, hp, energy, final_damage):
     base_dmg = (level + damage) * (level / 10)
     hp_influence = ((hp / 20) * (energy / 40)) * 0.2
@@ -20,7 +26,8 @@ def calculate_damage_swan(level, damage, hp, energy, final_damage):
 
 # Dictionary for function mapping
 damage_calculators = {
-    "1": calculate_damage_bishop,
-    "2": calculate_damage_dark_knight,
-    "3": calculate_damage_swan
+    "1": calculate_damage_assassin,
+    "2": calculate_damage_bishop,
+    "3": calculate_damage_dark_knight,
+    "4": calculate_damage_swan
 }
